425b5cb66265e93cab2f7321eac8a3e9e0942ba4
赖巍 <skyoflw@gmail.com>
2015-01-13 17:09:20 +0800

    1. 添加了两个基础的方法，提供简化的事件触发及性能测试方法。

---
+++ Jasily.Test/JasilyPerformanceTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Test
+{
+    public sealed class JasilyPerformanceTest
+    {
+        Stopwatch Watcher;
+        int ListenId = 184;
+
+        public event EventHandler<JasilyPerformanceTestHitEventArgs> NextEvent;
+
+        JasilyPerformanceTest(int listenId)
+        {
+            ListenId = listenId;
+            Init();
+        }
+
+        [Conditional("DEBUG")]
+        private void Init()
+        {
+            Watcher = new Stopwatch();
+        }
+
+        [Conditional("DEBUG")]
+        public void Start()
+        {
+            Watcher.Start();
+        }
+
+        [Conditional("DEBUG")]
+        public void Next(int id, string desc)
+        {
+            if (id != ListenId) return;
+            var time = Watcher.ElapsedMilliseconds;
+            NextEvent.Fire(this, new JasilyPerformanceTestHitEventArgs(id, desc, time));
+            Watcher.Restart();
+        }
+
+        public sealed class JasilyPerformanceTestHitEventArgs
+        {
+            public JasilyPerformanceTestHitEventArgs(int id, string desc, long milliseconds)
+            {
+                Id = id;
+                Description = desc;
+                Milliseconds = milliseconds;
+            }
+
+            public long Milliseconds { get; private set; }
+
+            public string Description { get; private set; }
+
+            public int Id { get; private set; }
+        }
+    }
+}
---
+++ Jasily/JasilyEvent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System
+{
+    public static class JasilyEvent
+    {
+        public static void Fire<T>(this EventHandler<T> e, object sender, T args)
+        {
+            if (e != null)
+                e(sender, args);
+        }
+    }
+}
