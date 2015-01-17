8b18d793a8942af7753f6502b00c1e054d6a5647
赖巍 <skyoflw@gmail.com>
2015-01-17 17:53:50 +0800

    1. 添加了 Throw 方法

---
+++ Jasily/JasilyThrow.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System
+{
+    public static class JasilyThrow
+    {
+        public static T ThrowIfNull<T>(this T obj, string paramName)
+        {
+            if (obj == null)
+                throw new ArgumentNullException(paramName);
+            return obj;
+        }
+    }
+}
