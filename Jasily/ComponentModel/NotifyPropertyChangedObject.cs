f1b4dab9adfcf941c8e9af7c13a8bf5346b1c659
赖巍 <skyoflw@gmail.com>
2015-01-17 21:46:42 +0800

    1. 添加了常用的列表 ObservableCollectionGroup.cs 2. 添加了常用的通知对象 NotifyPropertyChangedObject.cs 3. 添加了常用的显示对 NameValuePair.cs

---
+++ Jasily/ComponentModel/NotifyPropertyChangedObject.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.ComponentModel
+{
+    public class NotifyPropertyChangedObject : INotifyPropertyChanged
+    {
+        public void SetPropertyRef<T>(ref T property, T newValue, string propertyName)
+        {
+            if ((property == null && newValue == null) || (property != null && property.Equals(newValue)))
+                return;
+
+            property = newValue;
+            PropertyChanged.Fire(this, propertyName);
+        }
+        public void SetPropertyRef<T>(ref T property, T newValue, params string[] propertyNames)
+        {
+            if ((property == null && newValue == null) || (property != null && property.Equals(newValue)))
+                return;
+            
+            property = newValue;
+            foreach (var propertyName in propertyNames)
+                PropertyChanged.Fire(this, propertyName);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+
+    public static class JasilyINotifyPropertyChangedEM
+    {
+        public static void Fire(this PropertyChangedEventHandler e, object sender, string propertyName)
+        {
+            if (e != null)
+                e(sender, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
