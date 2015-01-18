63543af01c037b7447ba27a8f9b117fe3dd8a276
赖巍 <skyoflw@gmail.com>
2015-01-18 16:51:29 +0800

    1. 添加了 CRC32 算法

---
+++ Jasily/Security/Cryptography/CRC32.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Security.Cryptography
+{
+    public abstract class CRC32 : HashAlgorithm
+    {
+        public static CRC32 Create()
+        {
+            return new CRC32CryptoServiceProvider();
+        }
+    }
+}
---
+++ Jasily/Security/Cryptography/JasilyHash.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Security.Cryptography
+{
+    public static class JasilyHash
+    {
+        public static string ConverterToHashString(this byte[] hashBytes)
+        {
+            return BitConverter.ToString(hashBytes).Replace("-", "").ToUpper();
+        }
+
+        public static string ComputeHashString(this HashAlgorithm hash, byte[] buffer)
+        {
+            return hash.ComputeHash(buffer).ConverterToHashString();
+        }
+        public static string ComputeHashString(this HashAlgorithm hash, Stream inputStream)
+        {
+            return hash.ComputeHash(inputStream).ConverterToHashString();
+        }
+        public static string ComputeHashString(this HashAlgorithm hash, byte[] buffer, int offset, int count)
+        {
+            return hash.ComputeHash(buffer, offset, count).ConverterToHashString();
+        }
+    }
+}
