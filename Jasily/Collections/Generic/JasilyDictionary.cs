60d37a450aa4229e69fd9d2bed801fccee6c57ec
赖巍 <skyoflw@gmail.com>
2015-01-20 10:22:41 +0800

    1. 添加了对字典的常用扩展方法

---
+++ Jasily/Collections/Generic/JasilyDictionary.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Collections.Generic
+{
+    public static class JasilyDictionary
+    {
+        public static TValue GetValueOrDefault<TKey, TValue>(this Dictionary<TKey, TValue> obj, TKey key, TValue defaultValue)
+        {
+            TValue r = default(TValue);
+
+            if (obj.TryGetValue(key, out r))
+                return r;
+            else
+                return defaultValue;
+        }
+        public static TResult GetValueOrDefault<TKey, TValue, TResult>(this Dictionary<TKey, TValue> obj, TKey key, Func<TValue, TResult> selector, TResult defaultValue)
+        {
+            TValue r = default(TValue);
+
+            if (obj.TryGetValue(key, out r))
+                return selector(r);
+            else
+                return defaultValue;
+        }
+    }
+}
