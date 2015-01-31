f4ef9c478fe6d85f7b65ec1840060d0680c44392
赖巍 <skyoflw@gmail.com>
2015-01-31 17:26:15 +0800

    1. 添加常用 ViewModel 子类 2. 添加文件系统测试方案

---
+++ Jasily/IO/FileSystemTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.IO
+{
+    public static class FileSystemTest
+    {
+        public static bool IsMatch(this FileSystemTestType type, string item)
+        {
+            return (type.HasFlag(FileSystemTestType.File) && File.Exists(item)) ||
+                   (type.HasFlag(FileSystemTestType.Directory) && Directory.Exists(item));
+        }
+
+        public static bool IsMatch(this FileSystemTestType type, IEnumerable<string> items)
+        {
+            if (items.Count() <= 1)
+                return items.All(z => type.IsMatch(z));
+            return type.HasFlag(FileSystemTestType.Multiple) && items.All(z => type.IsMatch(z));
+        }
+
+        public static IEnumerable<string> Filter(this FileSystemTestType type, IEnumerable<string> items)
+        {
+            if (type.HasFlag(FileSystemTestType.Multiple))
+                return items.Where(z => type.IsMatch(z));
+            else
+                return new string[] { };
+        }
+    }
+}
---
+++ Jasily/IO/FileSystemTestType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.IO
+{
+    [Flags]
+    public enum FileSystemTestType
+    {
+        None = 0,
+
+        File = 1,
+
+        Directory = 2,
+
+        Multiple = 4
+    }
+}
