bdc05eb5020d1eeebd8a599e70cdac8f10c2f07d
赖巍 <skyoflw@gmail.com>
2015-03-16 13:36:28 +0800

    1. create wp 8.0 & 8.1 projects

---
+++ Jasily.Core.CSShared/Windows/Media/Animation/JasilyStoryboard.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Windows.Media.Animation
+{
+#if WINDOWS_PHONE_80
+    public static class JasilyStoryboard
+    {
+        public static Task BeginAsync(this Storyboard storyboard)
+        {
+            storyboard.ThrowIfNull("storyboard");
+
+            var tcs = new TaskCompletionSource<bool>();
+
+            EventHandler onComplete = null;
+            onComplete  = (s, e) =>
+            {
+                // sure ref set to null.
+                storyboard.Completed -= onComplete;
+                tcs.SetResult(true);
+            };
+            storyboard.Completed += onComplete;
+
+            storyboard.Begin();
+
+            return tcs.Task;
+        }
+    }
+#endif
+}
--- Jasily.Core/JasilyThrow.cs
+++ Jasily.Core/JasilyThrow.cs
@@ -3,10 +3,25 @@ namespace System
 {
     public static class JasilyThrow
     {
-        public static T ThrowIfNull<T>(this T obj, string paramName)
+        /// <summary>
+        /// throw if current object is null.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        /// <param name="paramName"></param>
+        /// <param name="message"></param>
+        /// <exception cref="System.ArgumentNullException">throw if current object is null.</exception>
+        /// <returns></returns>
+        public static T ThrowIfNull<T>(this T obj, string paramName, string message = null)
         {
             if (obj == null)
-                throw new ArgumentNullException(paramName);
+            {
+                if (message == null)
+                    throw new ArgumentNullException(paramName);
+                else
+                    throw new ArgumentNullException(paramName, message);
+            }
+
             return obj;
         }
     }
