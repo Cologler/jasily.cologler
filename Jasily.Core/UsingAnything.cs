6e3ec72bb3ed3b58979757dba1c8b668bbcca333
赖巍 <skyoflw@gmail.com>
2015-05-07 17:15:53 +0800

    1. moditied using anything

---
+++ Jasily.Core/UsingAnything.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System
+{
+    public struct UsingAnything : IDisposable
+    {
+        Action StartAction;
+        Action EndAction;
+
+        public UsingAnything(Action start, Action end)
+        {
+            this.StartAction = start;
+            this.EndAction = end;
+        }
+
+        public void Start()
+        {
+            if (this.StartAction != null)
+            {
+                this.StartAction();
+            }
+        }
+
+        public void Dispose()
+        {
+            if (this.EndAction != null)
+            {
+                this.EndAction();
+            }
+        }
+    }
+}
