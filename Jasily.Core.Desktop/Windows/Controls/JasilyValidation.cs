7254167d3a066ef42235e7571ae7b9ac287c1915
赖巍 <skyoflw@gmail.com>
2015-05-18 17:59:37 +0800

    1. added Jasily.Core.Desktop/Windows/Controls/JasilyValidation.cs

---
+++ Jasily.Core.Desktop/Windows/Controls/JasilyValidation.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+using System.Linq;
+using System.Windows.Input;
+
+namespace System.Windows.Controls
+{
+    public static class JasilyValidation
+    {
+        public static bool IsValid(this DependencyObject node, bool isFocusErrorControl = false)
+        {
+            if (node == null)
+                return true;
+
+            bool hasError = Validation.GetHasError(node);
+
+            if (hasError)
+            {
+                if (isFocusErrorControl)
+                {
+                    node.TypeOf<IInputElement, IInputElement>(Keyboard.Focus);
+                }
+
+                return false;
+            }
+
+            return LogicalTreeHelper.GetChildren(node).OfType<DependencyObject>().All(subnode => IsValid(subnode, isFocusErrorControl));
+        }
+    }
+}
