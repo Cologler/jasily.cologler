341a47c14b42ed764169b27465c90db3b2da6a1b
赖巍 <skyoflw@gmail.com>
2015-05-19 10:12:16 +0800

    1. added CloneableAttribute.cs

---
+++ Jasily.Core/Attributes/CloneableAttribute.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+
+namespace System.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Field | AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
+    public class CloneableAttribute : Attribute
+    {
+        public static T CloneWithCloneableAttribute<T>(T source, T dest)
+        {
+            var type = typeof (T);
+            var fields = type.GetRuntimeFields();
+            foreach (var fieldInfo in fields)
+            {
+                var attr = fieldInfo.GetCustomAttribute<CloneableAttribute>();
+                if (attr != null)
+                {
+                    fieldInfo.SetValue(dest, fieldInfo.GetValue(source));
+                }
+            }
+            var properties = type.GetRuntimeProperties();
+            foreach (var propertyInfo in properties)
+            {
+                var attr = propertyInfo.GetCustomAttribute<CloneableAttribute>();
+                if (attr != null)
+                {
+                    propertyInfo.SetValue(dest, propertyInfo.GetValue(source));
+                }
+            }
+            return dest;
+        }
+    }
+}
