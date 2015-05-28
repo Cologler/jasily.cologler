9916cc8ad7c34edce41ee0dcadc62309762a8138
赖巍 <skyoflw@gmail.com>
2015-05-28 09:35:12 +0800

    1. added JasilyICollection.cs

---
+++ Jasily.Core/Collections/Generic/JasilyICollection.cs
@@ -0,0 +1,11 @@
+﻿namespace System.Collections.Generic
+{
+    public static class JasilyICollection
+    {
+        public static T Append<T>(this ICollection<T> collection, T item)
+        {
+            collection.Add(item);
+            return item;
+        }
+    }
+}
