1ec4bbe6db6bf813586dd75b40857d8dc1397049
赖巍 <skyoflw@gmail.com>
2015-05-29 18:02:17 +0800

    1. datetime function

---
+++ Jasily.Core/JasilyDateTime.cs
@@ -0,0 +1,12 @@
+﻿namespace System
+{
+    public static class JasilyDateTime
+    {
+        public static readonly DateTime DateTime1970 = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+
+        public static TimeSpan ToUnixTimeSpan(this DateTime dt)
+        {
+            return dt - DateTime1970;
+        }
+    }
+}
---
+++ Jasily.Core/UfDateTimeConverter.cs
@@ -0,0 +1,17 @@
+﻿namespace System
+{
+    public static class UfDateTimeConverter
+    {
+        public static readonly DateTime DateTime1970 = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+
+        public static DateTime UnixMillisecondsToDateTime(long milliseconds)
+        {
+            return DateTime1970.AddMilliseconds(milliseconds);
+        }
+
+        public static TimeSpan UnixMillisecondsToTimeSpan(long milliseconds)
+        {
+            return TimeSpan.FromMilliseconds(milliseconds);
+        }
+    }
+}
