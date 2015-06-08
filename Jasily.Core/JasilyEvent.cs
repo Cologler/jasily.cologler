1a4c64854e95e7dafc1ccb170cd9062eac6a5f69
赖巍 <skyoflw@gmail.com>
2015-06-08 16:05:18 +0800

    1. added event extend method

--- Jasily.Core/JasilyEvent.cs
+++ Jasily.Core/JasilyEvent.cs
@@ -53,6 +53,80 @@ namespace System
         /// <param name="e"></param>
         /// <param name="sender"></param>
         /// <param name="arg"></param>
+        public static async Task FireAsync(this EventHandler e, object sender, EventArgs arg = null)
+        {
+            if (e != null)
+                await Task.Run(() => e(sender, arg));
+        }
+        /// <summary>
+        /// if e != null, call e() with mulit args
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="e"></param>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        public static async Task FireAsync(this EventHandler e, object sender, params EventArgs[] args)
+        {
+            if (e != null)
+                await Task.Run(() => e.Fire(sender, args));
+        }
+        /// <summary>
+        /// if e != null, call e() with mulit args
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="e"></param>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        public static async Task FireAsync(this EventHandler e, object sender, IEnumerable<EventArgs> args)
+        {
+            if (e != null)
+                await Task.Run(() => e.Fire(sender, args.ToArray()));
+        }
+
+        /// <summary>
+        /// if e != null, call e() with arg
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="e"></param>
+        /// <param name="sender"></param>
+        /// <param name="arg"></param>
+        public static async void BeginFire(this EventHandler e, object sender, EventArgs arg = null)
+        {
+            if (e != null)
+                await Task.Run(() => e(sender, arg));
+        }
+        /// <summary>
+        /// if e != null, call e() with mulit args
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="e"></param>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        public static async void BeginFire(this EventHandler e, object sender, params EventArgs[] args)
+        {
+            if (e != null)
+                await Task.Run(() => e.Fire(sender, args));
+        }
+        /// <summary>
+        /// if e != null, call e() with mulit args
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="e"></param>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        public static async void BeginFire(this EventHandler e, object sender, IEnumerable<EventArgs> args)
+        {
+            if (e != null)
+                await Task.Run(() => e.Fire(sender, args.ToArray()));
+        }
+
+        /// <summary>
+        /// if e != null, call e() with arg
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="e"></param>
+        /// <param name="sender"></param>
+        /// <param name="arg"></param>
         public static void Fire<T>(this EventHandler<T> e, object sender, T arg)
         {
             if (e != null)
