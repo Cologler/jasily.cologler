df70a84393a7b4d499d2da0027380537723b3d84
赖巍 <skyoflw@gmail.com>
2015-07-05 18:47:42 +0800

    1. added ByteSize.cs

--- Jasily.Core/Collections/Generic/NameValuePair.cs
+++ Jasily.Core/Collections/Generic/NameValuePair.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace System.Collections.Generic
 {
-    public struct NameValuePair<TName, TValue>
+    public struct NameValuePair<TName, TValue> : IEquatable<NameValuePair<TName, TValue>>
     {
         private TName _name;
         private TValue _value;
@@ -15,6 +15,21 @@ namespace System.Collections.Generic
         public TName Name { get { return this._name; } }
 
         public TValue Value { get { return this._value; } }
+
+        /// <summary>
+        /// 指示当前对象是否等于同一类型的另一个对象。
+        /// </summary>
+        /// <returns>
+        /// 如果当前对象等于 <paramref name="other"/> 参数，则为 true；否则为 false。
+        /// </returns>
+        /// <param name="other">与此对象进行比较的对象。</param>
+        public bool Equals(NameValuePair<TName, TValue> other)
+        {
+            if (ReferenceEquals(this.Name, other.Name) && ReferenceEquals(this.Value, other.Value))
+                return true;
+
+            return Equals(this.Name, other.Name) && Equals(this.Value, other.Value);
+        }
 
         public override string ToString()
         {
---
+++ Jasily.Core/Enums/ByteSize.cs
@@ -0,0 +1,43 @@
+﻿namespace System.Enums
+{
+    public struct ByteSize
+    {
+        private readonly ByteSizeType unit;
+        private readonly double value;
+        private readonly long originValue;
+
+        public long OriginValue
+        {
+            get { return this.originValue; }
+        }
+
+        public ByteSizeType Unit
+        {
+            get { return this.unit; }
+        }
+
+        public double Value
+        {
+            get { return this.value; }
+        }
+
+        public ByteSize(ByteSizeType unit, double value, long originValue)
+            : this()
+        {
+            this.value = value;
+            this.originValue = originValue;
+            this.unit = unit;
+        }
+
+        /// <summary>
+        /// 返回该实例的完全限定类型名。
+        /// </summary>
+        /// <returns>
+        /// 包含完全限定类型名的 <see cref="T:System.String"/>。
+        /// </returns>
+        public override string ToString()
+        {
+            return String.Format("{0,-6:###.000} {1}", this.Value, this.Unit);
+        }
+    }
+}
