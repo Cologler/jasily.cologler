56f9ca9f0945d90d9e8ce26a0614e89b69951abd
赖巍 <skyoflw@gmail.com>
2015-07-24 04:34:40 +0800

    added http resource.

--- Jasily.Core/Net/HttpWebRequestResourceString.cs
+++ Jasily.Core/Net/HttpWebRequestResourceString.cs
@@ -3,12 +3,24 @@ namespace System.Net
 {
     public static class HttpWebRequestResourceString
     {
+	    public static class Method
+	    {
+            public const string Get = "GET";
+            public const string Post = "POST";
+            public const string Put = "PUT";
+        }
+
         public static class ContentType
         {
             /// <summary>
             /// return "application/json"
             /// </summary>
             public const string ApplicationJson = "application/json";
+
+            /// <summary>
+            /// return "application/x-www-form-urlencoded"
+            /// </summary>
+	        public const string ApplicationXWwwFormUrlencoded = "application/x-www-form-urlencoded";
 
             /// <summary>
             /// return "charset=utf-8"
