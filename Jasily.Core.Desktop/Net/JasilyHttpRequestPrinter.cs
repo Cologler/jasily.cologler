b9c68dba234e637cb6805d73fb21b21b9ac2eb50
赖巍 <skyoflw@gmail.com>
2015-07-25 00:47:09 +0800

    1. added buffer 2. added request printer

---
+++ Jasily.Core.Desktop/Net/JasilyHttpRequestPrinter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Net
+{
+    public class JasilyHttpRequestPrinter
+    {
+        public Action<string> LineWriter { get; set; }
+#if DEBUG
+            = WriteToDebug;
+#endif
+
+        private static void WriteToDebug(string line)
+        {
+            Debug.WriteLine(line);
+        }
+
+        public void Print(HttpListenerRequest request)
+        {
+            var writer = this.LineWriter;
+            if (writer == null) return;
+
+            writer($"{request.HttpMethod} {request.RawUrl} HTTP/{request.ProtocolVersion}");
+            foreach (var key in request.Headers.AllKeys)
+                writer($"[Header] {key} = {request.Headers[key]}");
+            writer("");
+        }
+    }
+}
---
+++ Jasily.Core/IO/JasilyBuffer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.IO
+{
+    public struct JasilyBuffer
+    {
+        public JasilyBuffer(byte[] buffer, int offset, int count)
+        {
+            this.Buffer = buffer;
+            this.Offset = offset;
+            this.Count = count;
+        }
+
+        public byte[] Buffer { get; }
+
+        public int Offset { get; }
+
+        public int Count { get; }
+    }
+}
