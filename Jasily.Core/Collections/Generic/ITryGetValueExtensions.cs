a14824da46bfd4271079bc1f8d64ec63f63eba17
赖巍 <skyoflw@gmail.com>
2015-07-28 04:11:35 +0800

    renamed

--- Jasily.Core/Collections/Generic/ITryGetValueExtensions.cs
+++ Jasily.Core/Collections/Generic/ITryGetValueExtensions.cs
@@ -1,26 +1,23 @@
 ﻿
 namespace System.Collections.Generic
 {
-    public static class JasilyITryGetValue
+    public static class ITryGetValueExtensions
     {
         public delegate bool TryGetVauleDelegate<in TKey, TValue>(TKey key, out TValue value);
 
-        public static TValue? GetValueOrNull<TKey, TValue>(
-            this ITryGetValue<TKey, TValue> obj, TKey key)
+        public static TValue? GetValueOrNull<TKey, TValue>(this ITryGetValue<TKey, TValue> obj, TKey key)
             where TValue : struct
         {
             return GetValueOrNull<TKey, TValue>(obj.TryGetValue, key);
         }
 
-        public static TValue GetValueOrDefault<TKey, TValue>(
-            this ITryGetValue<TKey, TValue> obj, TKey key,
+        public static TValue GetValueOrDefault<TKey, TValue>(this ITryGetValue<TKey, TValue> obj, TKey key,
             TValue defaultValue = default(TValue))
         {
             return GetValueOrDefault(obj.TryGetValue, key, defaultValue);
         }
 
-        public static TResult GetValueOrDefault<TKey, TValue, TResult>(
-            this ITryGetValue<TKey, TValue> obj, TKey key,
+        public static TResult GetValueOrDefault<TKey, TValue, TResult>(this ITryGetValue<TKey, TValue> obj, TKey key,
             Func<TValue, TResult> selector, TResult defaultValue = default(TResult))
         {
             return GetValueOrDefault(obj.TryGetValue, key, selector, defaultValue);
@@ -32,15 +29,10 @@ namespace System.Collections.Generic
             if (func == null) throw new ArgumentNullException("func");
 
             TValue r;
-
-            if (func(key, out r))
-                return r;
-            else
-                return null;
+            return func(key, out r) ? r : (TValue?) null;
         }
 
-        public static TValue GetValueOrDefault<TKey, TValue>(
-            this TryGetVauleDelegate<TKey, TValue> func, TKey key,
+        public static TValue GetValueOrDefault<TKey, TValue>(this TryGetVauleDelegate<TKey, TValue> func, TKey key,
             TValue defaultValue = default(TValue))
         {
             if (func == null) throw new ArgumentNullException("func");
@@ -50,26 +42,22 @@ namespace System.Collections.Generic
             return func(key, out r) ? r : defaultValue;
         }
 
-        public static TValue GetValueOrDefault<TKey, TValue>(
-            this TryGetVauleDelegate<TKey, TValue> func, TKey key,
+        public static TValue GetValueOrDefault<TKey, TValue>(this TryGetVauleDelegate<TKey, TValue> func, TKey key,
             Func<TValue> defaultValueFunc)
         {
             if (func == null) throw new ArgumentNullException("func");
             if (defaultValueFunc == null) throw new ArgumentNullException("defaultValueFunc");
 
             TValue r;
-
             return func(key, out r) ? r : defaultValueFunc();
         }
 
-        public static TResult GetValueOrDefault<TKey, TValue, TResult>(
-            this TryGetVauleDelegate<TKey, TValue> func, TKey key,
+        public static TResult GetValueOrDefault<TKey, TValue, TResult>(this TryGetVauleDelegate<TKey, TValue> func, TKey key,
             Func<TValue, TResult> selector, TResult defaultResult = default(TResult))
         {
             if (func == null) throw new ArgumentNullException("func");
 
             TValue r;
-
             return func(key, out r) ? selector(r) : defaultResult;
         }
     }
