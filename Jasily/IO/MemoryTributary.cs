396829969924982706bc774f7f3a4a834db6a69e
赖巍 <skyoflw@gmail.com>
2015-08-01 02:10:22 +0800

    renamed

--- Jasily/IO/MemoryTributary.cs
+++ Jasily/IO/MemoryTributary.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
-namespace System.IO
+namespace Jasily.IO
 {
     /// <summary>
     /// source: http://memorytributary.codeplex.com/
