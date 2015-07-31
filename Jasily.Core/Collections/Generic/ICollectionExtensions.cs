afeae717dbffb2f4048a646d69f19ee4eaa18a81
赖巍 <skyoflw@gmail.com>
2015-07-31 21:49:47 +0800

    check args

--- Jasily.Core/Collections/Generic/ICollectionExtensions.cs
+++ Jasily.Core/Collections/Generic/ICollectionExtensions.cs
@@ -6,6 +6,7 @@ namespace System.Collections.Generic
     {
         public static void AddRange<T>(this ICollection<T> collection, params T[] items)
         {
+            if (collection == null) throw new ArgumentNullException(nameof(collection));
             if (items == null) throw new ArgumentNullException(nameof(items));
 
             foreach (var item in items)
@@ -15,6 +16,7 @@ namespace System.Collections.Generic
         }
         public static void AddRange<T>(this ICollection<T> collection, IEnumerable<T> items)
         {
+            if (collection == null) throw new ArgumentNullException(nameof(collection));
             if (items == null) throw new ArgumentNullException(nameof(items));
 
             foreach (var item in items)
@@ -25,27 +27,37 @@ namespace System.Collections.Generic
 
         public static ICollection<T> Append<T>(this ICollection<T> collection, T item)
         {
+            if (collection == null) throw new ArgumentNullException(nameof(collection));
+
             collection.Add(item);
             return collection;
         }
         public static ICollection<T> Append<T>(this ICollection<T> collection, params T[] items)
         {
+            if (collection == null) throw new ArgumentNullException(nameof(collection));
+
             collection.AddRange(items);
             return collection;
         }
         public static ICollection<T> Append<T>(this ICollection<T> collection, IEnumerable<T> items)
         {
+            if (collection == null) throw new ArgumentNullException(nameof(collection));
+
             collection.AddRange(items);
             return collection;
         }
 
         public static T AddAndReturn<T>(this ICollection<T> collection, T item)
         {
+            if (collection == null) throw new ArgumentNullException(nameof(collection));
+
             collection.Add(item);
             return item;
         }
         public static IEnumerable<T> AddAndReturn<T>(this ICollection<T> collection, IEnumerable<T> items)
         {
+            if (collection == null) throw new ArgumentNullException(nameof(collection));
+
             // ReSharper disable once PossibleMultipleEnumeration
             collection.AddRange(items);
             // ReSharper disable once PossibleMultipleEnumeration
@@ -54,15 +66,22 @@ namespace System.Collections.Generic
 
         public static int RemoveRange<T>(this ICollection<T> collection, params T[] items)
         {
+            if (collection == null) throw new ArgumentNullException(nameof(collection));
+
             return items.Count(collection.Remove);
         }
         public static int RemoveRange<T>(this ICollection<T> collection, IEnumerable<T> items)
         {
+            if (collection == null) throw new ArgumentNullException(nameof(collection));
+
             return items.Count(collection.Remove);
         }
 
         public static bool MoveTo<T>(this ICollection<T> source, T item, ICollection<T> dest)
         {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+            if (dest == null) throw new ArgumentNullException(nameof(dest));
+
             if (source.Remove(item))
             {
                 dest.Add(item);
