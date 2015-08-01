f516164d155d472fabebff9d0a7d7ebd11c209cf
赖巍 <skyoflw@gmail.com>
2015-08-01 12:39:18 +0800

    added JasilyCustomAttributeExtensions

---
+++ Jasily.Core/Reflection/JasilyCustomAttributeExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace System.Reflection
+{
+    public static class JasilyCustomAttributeExtensions
+    {
+        public static bool HasCustomAttribute<T>(this Module element) where T : Attribute
+            => element.GetCustomAttribute<T>() != null;
+
+        public static bool HasCustomAttribute<T>(this ParameterInfo element) where T : Attribute
+            => element.GetCustomAttribute<T>() != null;
+
+        public static bool HasCustomAttribute<T>(this MemberInfo element) where T : Attribute
+            => element.GetCustomAttribute<T>() != null;
+
+        public static bool HasCustomAttribute<T>(this Assembly element) where T : Attribute
+            => element.GetCustomAttribute<T>() != null;
+
+        public static bool HasCustomAttribute<T>(this ParameterInfo element, bool inherit) where T : Attribute
+            => element.GetCustomAttribute<T>() != null;
+
+        public static bool HasCustomAttribute<T>(this MemberInfo element, bool inherit) where T : Attribute
+            => element.GetCustomAttribute<T>() != null;
+    }
+}
