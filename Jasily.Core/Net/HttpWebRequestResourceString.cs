64df3a2ac7619092d1ab4680952ff43631d23254
赖巍 <skyoflw@gmail.com>
2015-08-13 00:30:04 +0800

    *

--- Jasily.Core/Net/HttpWebRequestResourceString.cs
+++ Jasily.Core/Net/HttpWebRequestResourceString.cs
@@ -3,8 +3,8 @@ namespace System.Net
 {
     public static class HttpWebRequestResourceString
     {
-	    public static class Method
-	    {
+        public static class Method
+        {
             public const string Get = "GET";
             public const string Post = "POST";
             public const string Put = "PUT";
@@ -13,20 +13,26 @@ namespace System.Net
 
         public static class ContentType
         {
-            /// <summary>
-            /// return "application/json"
-            /// </summary>
-            public const string ApplicationJson = "application/json";
+            public static class Application
+            {
+                /// <summary>
+                /// return "application/json"
+                /// </summary>
+                public const string Json = "application/json";
 
-            /// <summary>
-            /// return "application/x-www-form-urlencoded"
-            /// </summary>
-	        public const string ApplicationXWwwFormUrlencoded = "application/x-www-form-urlencoded";
+                /// <summary>
+                /// return "application/x-www-form-urlencoded"
+                /// </summary>
+                public const string XWwwFormUrlencoded = "application/x-www-form-urlencoded";
+            }
 
-            /// <summary>
-            /// return "charset=utf-8"
-            /// </summary>
-            public const string CharSetUtf8 = "charset=utf-8";
+            public static class CharSet
+            {
+                /// <summary>
+                /// return "charset=utf-8"
+                /// </summary>
+                public const string Utf8 = "charset=utf-8";
+            }
         }
     }
 }
