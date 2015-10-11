6612748b98b52ff0b4db27b53ad55affbf005e3f
赖巍 <skyoflw@gmail.com>
2015-10-11 14:55:57 +0800

    map extension names

--- Jasily/Data/JasilyExtensionName.cs
+++ Jasily/Data/JasilyExtensionName.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Jasily.Data
@@ -28,45 +29,49 @@ namespace Jasily.Data
             JPG, JPEG, PNG, GIF
         }
 
-        public static bool IsVideo(string extensionName)
+        public static bool IsVideo(string extensionName) => IsType<Video>(extensionName);
+
+        public static bool IsMusic(string extensionName) => IsType<Music>(extensionName);
+
+        public static bool IsPicture(string extensionName) => IsType<Picture>(extensionName);
+
+        private static bool IsType<T>(string extensionName) => ExtensionNameMapper<T>.Instance[extensionName];
+
+        private class ExtensionNameMapper<T>
         {
-            if (extensionName == null)
-                throw new ArgumentNullException();
+            public static ExtensionNameMapper<T> Instance { get; }
 
-            if (extensionName.IsNullOrWhiteSpace())
-                return false;
+            static ExtensionNameMapper()
+            {
+                Instance = new ExtensionNameMapper<T>();
+            }
 
-            return IsType<Video>(extensionName);
-        }
+            public IReadOnlyList<string> ExtensionNames { get; }
 
-        public static bool IsMusic(string extensionName)
-        {
-            if (extensionName == null)
-                throw new ArgumentNullException();
+            private ExtensionNameMapper()
+            {
+                this.ExtensionNames = Enum.GetValues(typeof(T)).OfType<T>().Select(z => z.ToString().ToLower()).ToList();
+            }
 
-            if (extensionName.IsNullOrWhiteSpace())
-                return false;
+            /// <summary>
+            /// return is extensionName was in this.ExtensionNames
+            /// </summary>
+            /// <param name="extensionName"></param>
+            /// <returns></returns>
+            public bool this[string extensionName]
+            {
+                get
+                {
+                    if (extensionName == null)
+                        throw new ArgumentNullException();
 
-            return IsType<Music>(extensionName);
-        }
+                    if (extensionName.IsNullOrWhiteSpace())
+                        return false;
 
-        public static bool IsPicture(string extensionName)
-        {
-            if (extensionName == null)
-                throw new ArgumentNullException();
-
-            if (extensionName.IsNullOrWhiteSpace())
-                return false;
-
-            return IsType<Picture>(extensionName);
-        }
-
-        private static bool IsType<T>(string extensionName)
-        {
-            extensionName = extensionName.TrimStart('.').ToLower();
-            return Enum.GetValues(typeof(T)).OfType<T>()
-                .Select(z => z.ToString().ToLower())
-                .Contains(extensionName);
+                    extensionName = extensionName.TrimStart('.').ToLower();
+                    return this.ExtensionNames.Contains(extensionName);
+                }
+            }
         }
     }
 }
