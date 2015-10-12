941cbfed488034d7b3483f948766ab139b970808
赖巍 <skyoflw@gmail.com>
2015-10-13 00:01:47 +0800

    added async enumerable extensions

--- Jasily.Core/Linq/AsyncEnumerableExtensions.cs
+++ Jasily.Core/Linq/AsyncEnumerableExtensions.cs
@@ -1,10 +1,55 @@
+using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace System.Linq
 {
     public static class AsyncEnumerableExtensions
     {
+        #region to
+
+        public static async Task<T[]> ToArrayAsync<T>(this IEnumerable<Task<T>> source)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+
+            return await Task.WhenAll(source);
+        }
+        public static async Task<T[]> ToArrayAsync<T>(this IEnumerable<Task<T>> source, CancellationToken token)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+
+            return (await source.ToListAsync(token)).ToArray();
+        }
+
+        public static async Task<List<T>> ToListAsync<T>(this IEnumerable<Task<T>> source)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+
+            var collection = source as ICollection;
+            var result = collection == null ? new List<T>() : new List<T>(collection.Count);
+            foreach (var item in source)
+            {
+                result.Add(await item);
+            }
+            return result;
+        }
+        public static async Task<List<T>> ToListAsync<T>(this IEnumerable<Task<T>> source, CancellationToken token)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+
+            var collection = source as ICollection;
+            var result = collection == null ? new List<T>() : new List<T>(collection.Count);
+            foreach (var item in source)
+            {
+                token.ThrowIfCancellationRequested();
+                result.Add(await item);
+            }
+            return result;
+        }
+
+        #endregion
+
         #region all or any
 
         public static async Task<bool> AllAsync<TSource>(this IEnumerable<TSource> source, Func<TSource, Task<bool>> predicateAsync)
