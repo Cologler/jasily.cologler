22ce67836e222f7d71eb4d527073e69ca92392d9
赖巍 <skyoflw@gmail.com>
2015-11-01 03:53:27 +0800

    added not null struct

---
+++ Jasily.Core/NotNull.cs
@@ -0,0 +1,27 @@
+﻿using JetBrains.Annotations;
+
+namespace System
+{
+    public struct NotNull<T> where T : class
+    {
+        [NotNull]
+        public T Value { get; }
+
+        private NotNull([NotNull] T value)
+        {
+            this.Value = value;
+        }
+
+        [NotNull]
+        public static implicit operator T(NotNull<T> source)
+        {
+            return source.Value;
+        }
+
+        public static explicit operator NotNull<T>([NotNull] T value)
+        {
+            if (value == null) throw new ArgumentNullException(nameof(value));
+            return new NotNull<T>(value);
+        }
+    }
+}
