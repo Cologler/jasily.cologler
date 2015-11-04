1ef27b4d0983c1a5f3bd3c38bd018d10d16b7ff1
赖巍 <skyoflw@gmail.com>
2015-11-04 21:19:07 +0800

    remove notnull<>

---
+++ Jasily.Core/WeakReferenceExtensions.cs
@@ -0,0 +1,22 @@
+﻿
+namespace System
+{
+    public static class WeakReferenceExtensions
+    {
+        /// <summary>
+        /// get target (return null if get failed) .
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="weakRef"></param>
+        /// <returns></returns>
+        public static T GetTargetOrNull<T>(this WeakReference<T> weakRef)
+            where T : class
+        {
+            T r;
+            if (weakRef.TryGetTarget(out r))
+                return r;
+            else
+                return null;
+        }
+    }
+}
