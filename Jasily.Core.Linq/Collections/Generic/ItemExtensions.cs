2d276d7f06fc904a8053de645c968113d1fd22fd
赖巍 <skyoflw@gmail.com>
2015-12-12 21:12:18 +0800

    added method

---
+++ Jasily.Core.Linq/Collections/Generic/ItemExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace System.Collections.Generic
+{
+    public static class ItemExtensions
+    {
+        public static T BeAdd<T>(this T obj, ICollection<T> collection)
+        {
+            collection.Add(obj);
+            return obj;
+        }
+    }
+}
