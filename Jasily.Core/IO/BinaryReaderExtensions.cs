ba004de68ba78ad80bdd420e0f3e7027ae007702
赖巍 <skyoflw@gmail.com>
2015-12-12 21:32:34 +0800

    added method to binary reader.

---
+++ Jasily.Core/IO/BinaryReaderExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace System.IO
+{
+    public static class BinaryReaderExtensions
+    {
+        public static byte[] ReadBytesOrThrow(this BinaryReader reader, int count)
+        {
+            var buffer = reader.ReadBytes(count);
+            if (buffer.Length != count) throw new EndOfStreamException();
+            return buffer;
+        }
+    }
+}
