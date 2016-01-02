6add394c6f5698e7ae4e8b5b88ed4d0fe20b835c
赖巍 <skyoflw@gmail.com>
2016-01-03 01:22:12 +0800

    added unit test

--- UnitTest.Desktop/UnitTestForString.cs
+++ UnitTest.Desktop/UnitTestForString.cs
@@ -44,5 +44,46 @@ namespace UnitTest.Desktop
             Assert.AreEqual("", "123456".Childs(-2, 2));
             Assert.AreEqual("3456", "123456".Childs(-4, -1));
         }
+
+        [TestMethod]
+        public void CommonStart()
+        {
+            var test1 = new string[] {};
+            Assert.AreEqual("", test1.CommonStart());
+
+            var test2 = new[] { "12" };
+            Assert.AreEqual("12", test2.CommonStart());
+
+            var test3 = new[] { "", "", "" };
+            Assert.AreEqual("", test3.CommonStart());
+
+            var test4 = new[] { "", "12", "" };
+            Assert.AreEqual("", test4.CommonStart());
+
+            var test5 = new[] { "12", "12", "123" };
+            Assert.AreEqual("12", test5.CommonStart());
+        }
+
+        [TestMethod]
+        public void CommonEnd()
+        {
+            var test1 = new string[] { };
+            Assert.AreEqual("", test1.CommonEnd());
+
+            var test2 = new[] { "12" };
+            Assert.AreEqual("12", test2.CommonEnd());
+
+            var test3 = new[] { "", "", "" };
+            Assert.AreEqual("", test3.CommonEnd());
+
+            var test4 = new[] { "", "12", "" };
+            Assert.AreEqual("", test4.CommonEnd());
+
+            var test5 = new[] { "12", "12", "123" };
+            Assert.AreEqual("", test5.CommonEnd());
+
+            var test6 = new[] { "012", "012", "312" };
+            Assert.AreEqual("12", test6.CommonEnd());
+        }
     }
 }
