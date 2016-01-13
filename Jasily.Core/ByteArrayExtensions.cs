d369b45756f643d55dc4163f952cc94de04dca8c
赖巍 <skyoflw@gmail.com>
2016-01-14 04:36:56 +0800

    refactor

--- Jasily.Core/ByteArrayExtensions.cs
+++ Jasily.Core/ByteArrayExtensions.cs
@@ -13,9 +13,7 @@ namespace System
         /// <param name="bytes"></param>
         /// <returns></returns>
         public static MemoryStream ToMemoryStream(this byte[] bytes)
-        {
-            return new MemoryStream(bytes, false);
-        }
+            => new MemoryStream(bytes, false);
 
         /// <summary>
         /// get string use special encoding
@@ -24,9 +22,7 @@ namespace System
         /// <param name="encoding"></param>
         /// <returns></returns>
         public static string GetString(this byte[] bytes, Encoding encoding)
-        {
-            return encoding.GetString(bytes, 0, bytes.Length);
-        }
+            => encoding.GetString(bytes, 0, bytes.Length);
 
         /// <summary>
         /// get string use encoding-utf8
@@ -34,28 +30,18 @@ namespace System
         /// <param name="bytes"></param>
         /// <returns></returns>
         public static string GetString(this byte[] bytes)
-        {
-            return bytes.GetString(Encoding.UTF8);
-        }
+            => bytes.GetString(Encoding.UTF8);
 
         public static string GetHexString(this IEnumerable<byte> bytes)
-        {
-            return BitConverter.ToString(bytes.ToArray());
-        }
+            => BitConverter.ToString(bytes.ToArray());
 
         public static string GetHexString(this byte[] bytes)
-        {
-            return BitConverter.ToString(bytes);
-        }
+            => BitConverter.ToString(bytes);
 
         public static string GetHexString(this byte[] bytes, int startIndex)
-        {
-            return BitConverter.ToString(bytes, startIndex);
-        }
+            => BitConverter.ToString(bytes, startIndex);
 
         public static string GetHexString(this byte[] bytes, int startIndex, int length)
-        {
-            return BitConverter.ToString(bytes, startIndex, length);
-        }
+            => BitConverter.ToString(bytes, startIndex, length);
     }
 }
