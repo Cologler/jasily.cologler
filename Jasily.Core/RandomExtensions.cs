13aa391f4c505bf16dcb6331c4f0f5faf8cee83a
赖巍 <skyoflw@gmail.com>
2016-01-14 04:33:53 +0800

    feat: make random once

--- Jasily.Core/RandomExtensions.cs
+++ Jasily.Core/RandomExtensions.cs
@@ -5,25 +5,34 @@ namespace System
 {
     public static class RandomExtensions
     {
-        public static T Random<T>(this T[] t)
+        private static Random randomNumberGenerator;
+
+        public static Random RandomNumberGenerator
+            => randomNumberGenerator ?? (randomNumberGenerator = new Random());
+
+        #region extension for collection
+
+        public static T Random<T>(this T[] t, Random random = null)
         {
             if (t.Length == 0) return default(T);
             if (t.Length == 1) return t[0];
-            return t[new Random().Next(t.Length)];
+            random = random ?? RandomNumberGenerator;
+            return t[random.Next(t.Length)];
         }
 
-        public static T Random<T>(this IList<T> t)
+        public static T Random<T>(this IList<T> t, Random random = null)
         {
             if (t.Count == 0) return default(T);
             if (t.Count == 1) return t[0];
-            return t[new Random().Next(t.Count)];
+            random = random ?? RandomNumberGenerator;
+            return t[random.Next(t.Count)];
         }
 
-        public static IEnumerable<T> RandomSort<T>(this IEnumerable<T> source)
+        public static IEnumerable<T> RandomSort<T>(this IEnumerable<T> source, Random random = null)
         {
             var array = source.ToArray();
             var count = array.Length;
-            var random = new Random();
+            random = random ?? RandomNumberGenerator;
             while (count > 0)
             {
                 var index = random.Next(count);
@@ -34,6 +43,8 @@ namespace System
             }
         }
 
+        #endregion
+
         public static byte[] NextBytes(this Random random, int byteCount)
         {
             var buffer = new byte[byteCount];
@@ -41,8 +52,8 @@ namespace System
             return buffer;
         }
 
-        public static long NextInt32(Random random) => BitConverter.ToInt32(random.NextBytes(4), 0);
+        public static int NextInt32(this Random random) => BitConverter.ToInt32(random.NextBytes(4), 0);
 
-        public static long NextInt64(Random random) => BitConverter.ToInt64(random.NextBytes(8), 0);
+        public static long NextInt64(this Random random) => BitConverter.ToInt64(random.NextBytes(8), 0);
     }
 }
