09c5e3f6d4c7291ab3b967c1382be9bc2daf2e07
赖巍 <skyoflw@gmail.com>
2016-01-16 08:54:47 +0800

    refactor

---
+++ Jasily.Core/DelegateExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+
+namespace System
+{
+    public static class DelegateExtensions
+    {
+        public static async Task InvokeAsync(this Action action) => await Task.Run(() => action.Invoke());
+
+        public static async void BeginInvoke(this Action action) => await InvokeAsync(action);
+
+        public static async Task<T> InvokeAsync<T>(this Func<T> func) => await Task.Run(() => func.Invoke());
+
+        public static async void BeginInvoke<T>(this Func<T> func) => await InvokeAsync(func);
+    }
+}
