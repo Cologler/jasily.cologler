b3808805aae9f21de9f191996487e4c908935403
赖巍 <skyoflw@gmail.com>
2016-01-16 08:00:28 +0800

    refactor

---
+++ Jasily.Core/TypeExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+
+namespace System
+{
+    public static class TypeExtensions
+    {
+        public static object CreateInstance(this Type type) => Activator.CreateInstance(type);
+
+        public static object CreateInstance(this Type type, params object[] args) => Activator.CreateInstance(type, args);
+
+        public static T CreateInstance<T>(this Type type) => (T)Activator.CreateInstance(type);
+
+        public static T CreateInstance<T>(this Type type, params object[] args) => (T)Activator.CreateInstance(type, args);
+
+        public static object GetDefaultValue(this Type type) => type.GetTypeInfo().IsValueType ? Activator.CreateInstance(type) : null;
+    }
+}
