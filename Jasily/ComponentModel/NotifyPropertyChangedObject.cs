d356ee9469fb155b85f3ff98194af134b79a3ca3
赖巍 <skyoflw@gmail.com>
2016-02-06 21:05:24 +0800

    feat: map

--- Jasily/ComponentModel/NotifyPropertyChangedObject.cs
+++ Jasily/ComponentModel/NotifyPropertyChangedObject.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using JetBrains.Annotations;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -75,18 +76,15 @@ namespace Jasily.ComponentModel
         /// </summary>
         public virtual void RefreshProperties()
         {
-            var properties = (
-                from property in this.GetType().GetRuntimeProperties()
-                let attr = property.GetCustomAttribute<NotifyPropertyChangedAttribute>()
-                where attr != null
-                orderby attr.Order
-                select property.Name
-            ).ToArray();
+            var properties = this.PropertiesMapper?.GetProperties(this) ??
+                MapNotifyPropertyChangedAttribute(this.GetType());
 
             this.NotifyPropertyChanged(properties);
 
             this.PropertiesRefreshed?.Invoke(this);
         }
+
+        public RefreshPropertiesMapper PropertiesMapper { get; set; }
 
         protected void NotifyPropertyChanged<T>(Expression<Func<T, object>> propertySelector)
         {
@@ -103,6 +101,37 @@ namespace Jasily.ComponentModel
         public void ClearPropertyChangedInvocationList()
         {
             this.PropertyChanged = null;
+        }
+
+        public sealed class RefreshPropertiesMapper
+        {
+            private readonly Type type;
+            private readonly string[] properties;
+
+            public RefreshPropertiesMapper([NotNull] Type type)
+            {
+                if (type == null) throw new ArgumentNullException(nameof(type));
+                this.type = type;
+                this.properties = MapNotifyPropertyChangedAttribute(type);
+            }
+
+            internal string[] GetProperties(NotifyPropertyChangedObject obj)
+            {
+                if (obj.GetType() != this.type) throw new InvalidOperationException();
+
+                return this.properties;
+            }
+        }
+
+        private static string[] MapNotifyPropertyChangedAttribute(Type type)
+        {
+            return (
+                from property in type.GetRuntimeProperties()
+                let attr = property.GetCustomAttribute<NotifyPropertyChangedAttribute>()
+                where attr != null
+                orderby attr.Order
+                select property.Name
+            ).ToArray();
         }
     }
 }
