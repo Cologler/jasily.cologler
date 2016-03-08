6bf6340446fe753ac14d154e6b2615d2c879a214
赖巍 <skyoflw@gmail.com>
2016-03-08 20:03:19 +0800

    feat: lock-free stack

--- Jasily/Collections/LockFreeCollections/LockFreeQueue.cs
+++ Jasily/Collections/LockFreeCollections/LockFreeQueue.cs
@@ -1,7 +1,4 @@
-using Jasily.Threading;
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Threading;
 
@@ -13,63 +10,33 @@ namespace Jasily.Collections.LockFreeCollections
     /// thread-safe with lock-free queue
     /// </summary>
     /// <typeparam name="T"></typeparam>
-    public class LockFreeQueue<T> : IEnumerable<T>, IDisposable
+    public sealed class LockFreeQueue<T> : BaseLockFreeCollection<T>
     {
-        private volatile QueueData header;
-        private volatile int prevAdd;
-        private volatile int count;
-        private volatile QueueData footer;
-        private volatile int isDisposed = 1;
+        private volatile Node header;
+        private volatile Node footer;
 
         public LockFreeQueue()
         {
-            this.footer = this.header = new QueueData();
+            this.footer = this.header = new Node();
         }
 
-        public bool Enqueue(T item)
+        public bool Enqueue(T item) => base.Add(item);
+
+        protected override void AddCore(T item)
         {
-            if (this.IsDisposed) return false;
-
-            Interlocked.Increment(ref this.prevAdd);
-
-            if (this.IsDisposed)
-            {
-                Interlocked.Decrement(ref this.prevAdd);
-                return false;
-            }
-
             while (true)
             {
                 var footer = this.footer;
                 if (footer.TrySetNext(item))
                 {
-                    this.footer = this.footer.Next;
+                    this.footer = footer.Next;
                     break;
                 }
             }
-
-            Interlocked.Decrement(ref this.prevAdd);
-            Interlocked.Increment(ref this.count);
-            return true;
         }
 
-        public bool IsDisposed => this.isDisposed < 1;
-
-        public bool MayHasNext => this.isDisposed + this.prevAdd + this.count > 0;
-
-        public bool TryDequeue(out T value)
+        protected override T GetCore()
         {
-            int count;
-            do
-            {
-                count = this.count;
-                if (count <= 0)
-                {
-                    value = default(T);
-                    return false;
-                }
-            } while (Interlocked.CompareExchange(ref this.count, count - 1, count) != count);
-
             while (true)
             {
                 var header = this.header;
@@ -87,125 +54,18 @@ namespace Jasily.Collections.LockFreeCollections
                 }
                 if (Interlocked.CompareExchange(ref this.header, next.Next, header) == header)
                 {
-                    value = next.Value;
-                    return true;
+                    return next.Value;
                 }
             }
         }
+
+        public bool TryDequeue(out T value)
+            => this.TryGet(out value);
 
         public bool TryDequeue(out T value, int millisecondsTimeout)
-        {
-            if (millisecondsTimeout == Timeout.Infinite)
-            {
-                while (!this.TryDequeue(out value)) { }
-                return true;
-            }
-            else if (millisecondsTimeout < 0)
-            {
-                throw new ArgumentOutOfRangeException();
-            }
-            else if (millisecondsTimeout == 0)
-            {
-                return this.TryDequeue(out value);
-            }
-            else
-            {
-
-                JasilyTimeout timeout = millisecondsTimeout;
-                while (true)
-                {
-                    var lt = timeout.LeftTime;
-                    if (lt < 0) break;
-                    if (lt == 0) return this.TryDequeue(out value);
-                    if (SpinWait.SpinUntil(() => this.Count > 0, lt) && this.TryDequeue(out value))
-                    {
-                        return true;
-                    }
-                }
-                value = default(T);
-                return false;
-            }
-        }
+            => this.TryGet(out value, millisecondsTimeout);
 
         public bool TryDequeue(out T value, TimeSpan timeout)
-            => this.TryDequeue(out value, (int)timeout.TotalMilliseconds);
-
-        public int Count => this.count;
-
-        private class QueueData
-        {
-            private QueueData next;
-
-            public T Value { get; }
-
-            public bool HasValue { get; }
-
-            public QueueData()
-            {
-                this.HasValue = false;
-            }
-
-            public QueueData(T value)
-            {
-                this.Value = value;
-                this.HasValue = true;
-            }
-
-            public QueueData Next => this.next;
-
-            public bool TrySetNext(T value)
-            {
-                if (this.next != null) return false;
-                return Interlocked.CompareExchange(ref this.next, new QueueData(value), null) == null;
-            }
-
-            public bool TrySetNextEmpty()
-            {
-                if (this.next != null) return false;
-                return Interlocked.CompareExchange(ref this.next, new QueueData(), null) == null;
-            }
-        }
-
-        public void Dispose()
-        {
-            this.isDisposed = 0;
-        }
-
-        public IEnumerator<T> GetEnumerator() => new Enumerator(this);
-
-        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
-
-        private class Enumerator : IEnumerator<T>
-        {
-            private readonly LockFreeQueue<T> queue;
-            private T value;
-
-            public Enumerator(LockFreeQueue<T> queue)
-            {
-                this.queue = queue;
-            }
-
-            public bool MoveNext()
-            {
-                while (this.queue.MayHasNext)
-                {
-                    if (this.queue.TryDequeue(out this.value)) return true;
-                }
-
-                return false;
-            }
-
-            public void Reset()
-            {
-            }
-
-            public T Current => this.value;
-
-            object IEnumerator.Current => this.Current;
-
-            public void Dispose()
-            {
-            }
-        }
+            => this.TryGet(out value, timeout);
     }
 }
--- UnitTest.Desktop/Collections/LockFreeCollections/UnitTestForLockFreeStack.cs
+++ UnitTest.Desktop/Collections/LockFreeCollections/UnitTestForLockFreeStack.cs
@@ -1,44 +1,44 @@
-﻿using Jasily.Collections.LockFreeCollections;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Jasily.Collections.LockFreeCollections;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace UnitTest.Desktop.Collections.LockFreeCollections
 {
     [TestClass]
-    public class UnitTestForLockFreeQueue
+    public class UnitTestForLockFreeStack
     {
         [TestMethod]
         public void TestMethod()
         {
-            var queue = new LockFreeQueue<int>();
+            var queue = new LockFreeStack<int>();
             int value;
-            Assert.AreEqual(false, queue.TryDequeue(out value));
-            queue.Enqueue(5);
-            Assert.AreEqual(true, queue.TryDequeue(out value));
+            Assert.AreEqual(false, queue.TryPop(out value));
+            queue.Push(5);
+            Assert.AreEqual(true, queue.TryPop(out value));
             Assert.AreEqual(5, value);
-            Assert.AreEqual(false, queue.TryDequeue(out value));
-            queue.Enqueue(9);
-            queue.Enqueue(10);
-            Assert.AreEqual(true, queue.TryDequeue(out value));
-            Assert.AreEqual(true, queue.TryDequeue(out value));
-            Assert.AreEqual(false, queue.TryDequeue(out value));
+            Assert.AreEqual(false, queue.TryPop(out value));
+            queue.Push(9);
+            queue.Push(10);
+            Assert.AreEqual(true, queue.TryPop(out value));
+            Assert.AreEqual(true, queue.TryPop(out value));
+            Assert.AreEqual(false, queue.TryPop(out value));
         }
 
         [TestMethod]
         public void TestMethodWithThread()
         {
-            var queue = new LockFreeQueue<int>();
+            var queue = new LockFreeStack<int>();
             var count = 100000;
             int value;
             var setter = new Action(() =>
             {
                 foreach (var v in Enumerable.Range(0, count))
                 {
-                    queue.Enqueue(v);
+                    queue.Push(v);
                 }
             });
 
@@ -47,7 +47,7 @@ namespace UnitTest.Desktop.Collections.LockFreeCollections
             {
                 while (queue.MayHasNext)
                 {
-                    if (queue.TryDequeue(out value)) Interlocked.Increment(ref total);
+                    if (queue.TryPop(out value)) Interlocked.Increment(ref total);
                 }
             });
 
@@ -58,7 +58,7 @@ namespace UnitTest.Desktop.Collections.LockFreeCollections
 
             Task.WaitAll(setters, getters);
 
-            queue = new LockFreeQueue<int>();
+            queue = new LockFreeStack<int>();
             setters = Task.WhenAll(Task.Run(setter), Task.Run(setter)).ContinueWith(z => queue.Dispose());
             getters = Task.WhenAll(Task.Run(getter), Task.Run(getter)).ContinueWith(z => Assert.AreEqual(count * 6, total));
             Task.WaitAll(setters, getters);
@@ -67,13 +67,13 @@ namespace UnitTest.Desktop.Collections.LockFreeCollections
         [TestMethod]
         public void TestMethodWithEnumerator()
         {
-            var queue = new LockFreeQueue<int>();
+            var queue = new LockFreeStack<int>();
             var count = 100000;
             var setter = new Action(() =>
             {
                 foreach (var v in Enumerable.Range(0, count))
                 {
-                    queue.Enqueue(v);
+                    queue.Push(v);
                 }
             });
 
@@ -93,7 +93,7 @@ namespace UnitTest.Desktop.Collections.LockFreeCollections
 
             Task.WaitAll(setters, getters);
 
-            queue = new LockFreeQueue<int>();
+            queue = new LockFreeStack<int>();
             setters = Task.WhenAll(Task.Run(setter), Task.Run(setter)).ContinueWith(z => queue.Dispose());
             getters = Task.WhenAll(Task.Run(getter), Task.Run(getter)).ContinueWith(z => Assert.AreEqual(count * 6, total));
             Task.WaitAll(setters, getters);
@@ -102,31 +102,31 @@ namespace UnitTest.Desktop.Collections.LockFreeCollections
         [TestMethod]
         public void TestMethodWithTimeout()
         {
-            var queue = new LockFreeQueue<int>();
+            var queue = new LockFreeStack<int>();
             int value;
 
             var now = DateTime.UtcNow;
-            Assert.AreEqual(false, queue.TryDequeue(out value, 1000));
+            Assert.AreEqual(false, queue.TryPop(out value, 1000));
             Debug.WriteLine((DateTime.UtcNow - now).TotalMilliseconds);
 
             Task.Run(async () =>
             {
                 await Task.Delay(5000);
-                queue.Enqueue(100);
+                queue.Push(100);
             });
             now = DateTime.UtcNow;
-            Assert.AreEqual(true, queue.TryDequeue(out value, 10000));
+            Assert.AreEqual(true, queue.TryPop(out value, 10000));
             Debug.WriteLine((DateTime.UtcNow - now).TotalMilliseconds);
 
             Task.Run(async () =>
             {
                 await Task.Delay(5000);
-                queue.Enqueue(100);
+                queue.Push(100);
             });
             now = DateTime.UtcNow;
-            Assert.AreEqual(false, queue.TryDequeue(out value, 2000));
+            Assert.AreEqual(false, queue.TryPop(out value, 2000));
             Debug.WriteLine((DateTime.UtcNow - now).TotalMilliseconds);
-            Assert.AreEqual(true, queue.TryDequeue(out value, 4000));
+            Assert.AreEqual(true, queue.TryPop(out value, 4000));
             Debug.WriteLine((DateTime.UtcNow - now).TotalMilliseconds);
         }
     }
