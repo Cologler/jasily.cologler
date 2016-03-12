e0d603e5c127021620d1046001d7f94a8490ca0f
赖巍 <skyoflw@gmail.com>
2016-03-12 16:59:21 +0800

    featL nullable hash set.

---
+++ Jasily/Container.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Jasily
+{
+    public sealed class Container<T>
+    {
+        private T value;
+
+        public void RemoveValue()
+        {
+            this.HasValue = false;
+            this.value = default(T);
+        }
+
+        public void SetValue(T value)
+        {
+            //this.value = value;
+            //this.HasValue = true;
+        }
+
+        public bool HasValue { get; private set; }
+
+        public T Value
+        {
+            get
+            {
+                if (this.HasValue) return this.value;
+                throw new InvalidOperationException();
+            }
+            set { this.SetValue(value); }
+        }
+    }
+}
