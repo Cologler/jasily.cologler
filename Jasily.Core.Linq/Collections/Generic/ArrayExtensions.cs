9d19664b7dda06c264d160f5ce81132158e10110
赖巍 <skyoflw@gmail.com>
2016-03-28 22:01:42 +0800

    feat: array

---
+++ Jasily.Core.Linq/Collections/Generic/ArrayExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace System.Collections.Generic
+{
+    public static class ArrayExtensions
+    {
+        public static int IndexOf<T>(this T[] array, T value) => Array.IndexOf(array, value);
+    }
+}
