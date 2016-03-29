f3897a8db995213e3905f1c9b8929c7e9c6962fe
赖巍 <skyoflw@gmail.com>
2016-03-29 22:49:51 +0800

    array

--- Jasily.Core.Linq/Collections/Generic/ArrayExtensions.cs
+++ Jasily.Core.Linq/Collections/Generic/ArrayExtensions.cs
@@ -3,5 +3,12 @@
     public static class ArrayExtensions
     {
         public static int IndexOf<T>(this T[] array, T value) => Array.IndexOf(array, value);
+
+        public static T[] ToArray<T>(this T[] array)
+        {
+            var ret = new T[array.Length];
+            Array.Copy(array, ret, array.Length);
+            return ret;
+        }
     }
 }
