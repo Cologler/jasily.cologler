9ad02bc481680c5194629fd9ac0cc1bf5aeb2774
赖巍 <skyoflw@gmail.com>
2016-04-06 13:26:34 +0800

    fix

--- Jasily.Core/Text/StringBuilderExtensions.cs
+++ Jasily.Core/Text/StringBuilderExtensions.cs
@@ -4,8 +4,10 @@ namespace System.Text
 {
     public static class StringBuilderExtensions
     {
-        public static StringBuilder Append([NotNull] this StringBuilder builder, [NotNull] string value, int startIndex)
+        public static StringBuilder Append([NotNull] this StringBuilder builder, string value, int startIndex)
         {
+            if (builder == null) throw new ArgumentNullException(nameof(builder));
+            if (value == null) return builder;
             if (value.Length < startIndex) throw new ArgumentOutOfRangeException(nameof(startIndex));
             return value.Length == startIndex ? builder : builder.Append(value, startIndex, value.Length - startIndex);
         }
