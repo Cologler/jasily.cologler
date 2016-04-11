ea4297d0c5dabc952229b80ce98190205416e317
赖巍 <skyoflw@gmail.com>
2016-04-11 11:32:05 +0800

    feat: new api

---
+++ Jasily/Threading/JasilyEnvironment.cs
@@ -0,0 +1,112 @@
+using System;
+using System.Threading;
+
+namespace Jasily.Threading
+{
+    /// <summary>
+    /// this is not thread safe
+    /// </summary>
+    public abstract class JasilyEnvironment
+    {
+        public int Count { get; }
+
+        public abstract int Current { get; }
+
+        protected JasilyEnvironment(int count)
+        {
+            if (count <= 0) throw new ArgumentOutOfRangeException(nameof(count));
+
+            this.Count = count;
+        }
+
+        public Result Enter()
+        {
+            if (this.OnGet())
+            {
+                var r = new Result(true);
+                r.Exited += this.R_Exited;
+                return r;
+            }
+            return new Result(false);
+        }
+
+        private void R_Exited(object sender, EventArgs e) => this.OnPut();
+
+        protected abstract bool OnGet();
+
+        protected abstract void OnPut();
+
+        public struct Result : IDisposable
+        {
+            public event EventHandler Exited;
+
+            public Result(bool get)
+            {
+                // ReSharper disable once ArrangeThisQualifier
+                Exited = null;
+                this.IsGet = get;
+            }
+
+            public bool IsGet { get; }
+
+            public void Dispose() => this.Exited?.Invoke(this);
+        }
+
+        /// <summary>
+        /// general use in UI thread.
+        /// </summary>
+        /// <param name="count"></param>
+        /// <returns></returns>
+        public static JasilyEnvironment CreateThreadNotSafe(int count = 1)
+            => new ThreadNotSafeEnvironment(count);
+
+        public static JasilyEnvironment CreateThreadSafe(int count = 1)
+            => new ThreadSafeEnvironment(count);
+
+        private class ThreadNotSafeEnvironment : JasilyEnvironment
+        {
+            private int current;
+
+            public ThreadNotSafeEnvironment(int count)
+                : base(count)
+            {
+            }
+
+            public override int Current => this.current;
+
+            protected override bool OnGet()
+            {
+                if (this.current >= this.Count) return false;
+                this.current++;
+                return true;
+            }
+
+            protected override void OnPut() => this.current--;
+        }
+
+        private class ThreadSafeEnvironment : JasilyEnvironment
+        {
+            private int current;
+
+            public ThreadSafeEnvironment(int count)
+                : base(count)
+            {
+            }
+
+            public override int Current => this.current;
+
+            protected override bool OnGet()
+            {
+                if (this.current >= this.Count) return false;
+                if (Interlocked.Increment(ref this.current) > this.Count)
+                {
+                    Interlocked.Decrement(ref this.current);
+                    return false;
+                }
+                return true;
+            }
+
+            protected override void OnPut() => Interlocked.Decrement(ref this.current);
+        }
+    }
+}
--- Jasily/Threading/Tasks/JasilyTask.cs
+++ Jasily/Threading/Tasks/JasilyTask.cs
@@ -1,17 +1,68 @@
 ﻿using JetBrains.Annotations;
 using System;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Jasily.Threading.Tasks
 {
+    /// <summary>
+    /// if you try create a ( async void BeginXXX() => await XXXAsync(); ) just use this class!
+    /// </summary>
     public static class JasilyTask
     {
-        public static async void Begin([NotNull] params Func<Task>[] tasks)
+        private static async Task RunTaskFactory([NotNull] Func<Task> taskFactory)
         {
-            if (tasks == null || tasks.Any(z => z == null)) throw new ArgumentNullException(nameof(tasks));
+            Debug.WriteLine("JasilyTask: run at" + DateTime.Now);
+            Debug.Assert(taskFactory != null);
 
-            foreach (var t in tasks) await t();
+            var t = taskFactory();
+            if (t.Status == TaskStatus.Created) t.Start();
+            await t;
+        }
+
+        public static async void Begin([NotNull] Func<Task> taskFactory)
+        {
+            if (taskFactory == null) throw new ArgumentNullException(nameof(taskFactory));
+
+            await RunTaskFactory(taskFactory);
+        }
+
+        /// <summary>
+        /// exec task same time
+        /// </summary>
+        /// <param name="taskFactorys"></param>
+        public static void Begin([NotNull] params Func<Task>[] taskFactorys)
+        {
+            if (taskFactorys == null || taskFactorys.Any(z => z == null))
+                throw new ArgumentNullException(nameof(taskFactorys));
+
+            foreach (var t in taskFactorys) Begin(t);
+        }
+
+        /// <summary>
+        /// orderly exec task one by one
+        /// </summary>
+        /// <param name="taskFactorys"></param>
+        public static async void BeginQueue([NotNull] params Func<Task>[] taskFactorys)
+        {
+            if (taskFactorys == null || taskFactorys.Any(z => z == null))
+                throw new ArgumentNullException(nameof(taskFactorys));
+
+            foreach (var t in taskFactorys) await RunTaskFactory(t);
+        }
+
+        /// <summary>
+        /// orderly exec task one by one
+        /// </summary>
+        /// <param name="taskFactorys"></param>
+        /// <returns></returns>
+        public static async Task StartQueue([NotNull] params Func<Task>[] taskFactorys)
+        {
+            if (taskFactorys == null || taskFactorys.Any(z => z == null))
+                throw new ArgumentNullException(nameof(taskFactorys));
+
+            foreach (var t in taskFactorys) await RunTaskFactory(t);
         }
     }
 }
