f6b67a984f1d93a98f680f72bec1cf9c538599bd
赖巍 <skyoflw@gmail.com>
2016-04-12 21:08:26 +0800

    feat: asyncLazy support

---
+++ Jasily/AsyncLazy.cs
@@ -0,0 +1,20 @@
+﻿using Jasily.Threading.Tasks;
+using JetBrains.Annotations;
+using System;
+using System.Threading.Tasks;
+
+namespace Jasily
+{
+    public sealed class AsyncLazy<T>
+    {
+        private readonly UniqueTask<T> task;
+
+        public AsyncLazy([NotNull] Func<Task<T>> initFunc)
+        {
+            if (initFunc == null) throw new ArgumentNullException(nameof(initFunc));
+            this.task = new UniqueTask<T>(initFunc);
+        }
+
+        public async Task<T> GetValue() => await this.task.Run();
+    }
+}
