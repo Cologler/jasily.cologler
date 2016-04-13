421db5853729029ccac33ab2c6e8856058e5063b
赖巍 <skyoflw@gmail.com>
2016-04-13 19:52:39 +0800

    feat.

---
+++ Jasily/StringConverter.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Jasily
+{
+    public static class StringConverter
+    {
+        public static string EncodeToAscii(string value)
+        {
+            var sb = new StringBuilder();
+            foreach (var c in value)
+            {
+                if (c > 127)
+                {
+                    sb.Append("\\u" + ((int)c).ToString("x4"));
+                }
+                else
+                {
+                    sb.Append(c);
+                }
+            }
+            return sb.ToString();
+        }
+
+        public static string DecodeFromAscii(string value)
+        {
+            return Regex.Replace(value, @"\\u(?<Value>[a-zA-Z0-9]{4})",
+                m => ((char)int.Parse(m.Groups["Value"].Value, NumberStyles.HexNumber)).ToString());
+        }
+    }
+}
