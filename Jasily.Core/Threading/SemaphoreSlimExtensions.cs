65d734de5441b3fc5c4ac9345157761d77547e74
赖巍 <skyoflw@gmail.com>
2016-04-14 18:51:40 +0800

    feat: power.

--- Jasily.Core/Threading/SemaphoreSlimExtensions.cs
+++ Jasily.Core/Threading/SemaphoreSlimExtensions.cs
@@ -10,11 +10,45 @@ namespace System.Threading
             return new Releaser(semaphore);
         }
 
+        public static async Task<Releaser> LockAsync(this SemaphoreSlim semaphore,
+            int millisecondsTimeout)
+        {
+            return await semaphore.WaitAsync(millisecondsTimeout)
+                ? new Releaser(semaphore)
+                : new Releaser();
+        }
+
+        public static async Task<Releaser> LockAsync(this SemaphoreSlim semaphore,
+            int millisecondsTimeout, CancellationToken cancellationToken)
+        {
+            return await semaphore.WaitAsync(millisecondsTimeout, cancellationToken)
+                ? new Releaser(semaphore)
+                : new Releaser();
+        }
+
+        public static async Task<Releaser> LockAsync(this SemaphoreSlim semaphore,
+            TimeSpan timeout)
+        {
+            return await semaphore.WaitAsync(timeout)
+                ? new Releaser(semaphore)
+                : new Releaser();
+        }
+
+        public static async Task<Releaser> LockAsync(this SemaphoreSlim semaphore,
+            TimeSpan timeout, CancellationToken cancellationToken)
+        {
+            return await semaphore.WaitAsync(timeout, cancellationToken)
+                ? new Releaser(semaphore)
+                : new Releaser();
+        }
+
         public struct Releaser : IDisposable
         {
             private readonly SemaphoreSlim semaphore;
 
             internal Releaser(SemaphoreSlim semaphore) { this.semaphore = semaphore; }
+
+            public bool IsEntered => this.semaphore != null;
 
             public void Dispose() => this.semaphore?.Release();
         }
