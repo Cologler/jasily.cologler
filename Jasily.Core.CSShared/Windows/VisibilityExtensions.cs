8ebb01c23f944ee0302ab6fb6da413ad015e5af2
赖巍 <skyoflw@gmail.com>
2016-04-17 19:49:02 +0800

    for uwp.

--- Jasily.Core.CSShared/Windows/VisibilityExtensions.cs
+++ Jasily.Core.CSShared/Windows/VisibilityExtensions.cs
@@ -1,28 +1,25 @@
-﻿
+﻿#if WINDOWS_UWP
+using Windows.UI.Xaml;
+#endif
+
 namespace System.Windows
 {
     public static class VisibilityExtensions
     {
-#if DESKTOP
+#if DESKTOP || WINDOWS_UWP
         /// <summary>
         /// 如果 True，返回 Visible，否则返回 Collapsed
         /// </summary>
         /// <param name="value"></param>
         /// <returns>value ? Visibility.Visible : Visibility.Collapsed;</returns>
-        public static Visibility VisibleIfTrue(this bool value)
-        {
-            return value ? Visibility.Visible : Visibility.Collapsed;
-        }
+        public static Visibility VisibleIfTrue(this bool value) => value ? Visibility.Visible : Visibility.Collapsed;
 
         /// <summary>
         /// 如果 True，返回 Collapsed，否则返回 Visible
         /// </summary>
         /// <param name="value"></param>
         /// <returns></returns>
-        public static Visibility CollapsedIfTrue(this bool value)
-        {
-            return (!value) ? Visibility.Visible : Visibility.Collapsed;
-        }
+        public static Visibility CollapsedIfTrue(this bool value) => value ? Visibility.Collapsed : Visibility.Visible;
 #endif
     }
 }
