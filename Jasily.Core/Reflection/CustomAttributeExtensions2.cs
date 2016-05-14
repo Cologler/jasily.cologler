cd01babd27f3803ff1b1117a3e91fcc1433f455d
赖巍 <skyoflw@gmail.com>
2016-05-14 14:15:25 +0800

    refactor.

---
+++ Jasily.Core/Reflection/CustomAttributeExtensions2.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+
+namespace System.Reflection
+{
+    public static class CustomAttributeExtensions2
+    {
+        public static bool HasCustomAttribute<T>(this Module element) where T : Attribute
+            => element.GetCustomAttributes<T>().Any();
+
+        public static bool HasCustomAttribute<T>(this ParameterInfo element) where T : Attribute
+            => element.GetCustomAttributes<T>().Any();
+
+        public static bool HasCustomAttribute<T>(this MemberInfo element) where T : Attribute
+            => element.GetCustomAttributes<T>().Any();
+
+        public static bool HasCustomAttribute<T>(this Assembly element) where T : Attribute
+            => element.GetCustomAttributes<T>().Any();
+
+        public static bool HasCustomAttribute<T>(this ParameterInfo element, bool inherit) where T : Attribute
+            => element.GetCustomAttributes<T>(inherit).Any();
+
+        public static bool HasCustomAttribute<T>(this MemberInfo element, bool inherit) where T : Attribute
+            => element.GetCustomAttributes<T>(inherit).Any();
+    }
+}
