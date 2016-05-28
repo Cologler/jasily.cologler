6cd2e96a21120f0b9db6747fdb5d5efa874606d3
赖巍 <skyoflw@gmail.com>
2016-05-28 23:32:52 +0800

    feat

---
+++ Jasily/Start.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+
+namespace Jasily
+{
+    public static class Start
+    {
+        public static void Retry([NotNull] Action action, uint time, bool aggregateError = false)
+        {
+            if (action == null) throw new ArgumentNullException(nameof(action));
+            if (time <= 0) throw new ArgumentOutOfRangeException(nameof(time));
+
+            List<Exception> errors = null;
+            while (time-- > 0)
+            {
+                try
+                {
+                    action();
+                    return;
+                }
+                catch (Exception e)
+                {
+                    if (time == 0 && !aggregateError)
+                    {
+                        if (errors == null) errors = new List<Exception>();
+                        errors.Add(e);
+                    }
+                    else
+                    {
+                        if (time == 0)
+                        {
+                            throw;
+                        }
+                    }
+                }
+            }
+            throw new AggregateException(errors);
+        }
+
+        public static T Retry<T>([NotNull] Func<T> action, uint time, bool aggregateError = false)
+        {
+            if (action == null) throw new ArgumentNullException(nameof(action));
+            if (time <= 0) throw new ArgumentOutOfRangeException(nameof(time));
+
+            List<Exception> errors = null;
+            while (time-- > 0)
+            {
+                try
+                {
+                    return action();
+                }
+                catch (Exception e)
+                {
+                    if (time == 0 && !aggregateError)
+                    {
+                        if (errors == null) errors = new List<Exception>();
+                        errors.Add(e);
+                    }
+                    else
+                    {
+                        if (time == 0)
+                        {
+                            throw;
+                        }
+                    }
+                }
+            }
+            throw new AggregateException(errors);
+        }
+    }
+}
