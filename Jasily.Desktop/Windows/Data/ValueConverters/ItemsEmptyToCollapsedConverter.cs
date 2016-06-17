3a5373c609233d7788106d3cda06772243b73ec1
赖巍 <skyoflw@gmail.com>
2016-06-17 22:21:34 +0800

    feat.

--- Jasily.Desktop/Windows/Data/ValueConverters/ItemsEmptyToCollapsedConverter.cs
+++ Jasily.Desktop/Windows/Data/ValueConverters/ItemsEmptyToCollapsedConverter.cs
@@ -1,14 +1,15 @@
-﻿using System;
+using System;
+using System.Collections;
 using System.Globalization;
+using System.Linq;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Data;
 
 namespace Jasily.Windows.Data.ValueConverters
 {
-    public class BooleanToVisibilityConverter2 : IValueConverter
+    public class ItemsEmptyToCollapsedConverter : IValueConverter
     {
-        #region Implementation of IValueConverter
-
         /// <summary>
         /// Converts a value. 
         /// </summary>
@@ -18,29 +19,30 @@ namespace Jasily.Windows.Data.ValueConverters
         /// <param name="value">The value produced by the binding source.</param><param name="targetType">The type of the binding target property.</param><param name="parameter">The converter parameter to use.</param><param name="culture">The culture to use in the converter.</param>
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            var bValue = value as bool? ?? false;
-            var arg = parameter as string ?? "";
-            switch (arg)
+            var list = value as IList;
+            var enumerable = value as IEnumerable;
+
+            if (list == null && enumerable == null)
             {
-                case "01":
-                    return bValue ? Visibility.Visible : Visibility.Hidden;
-
-                case "02":
-                default:
-                    return bValue ? Visibility.Visible : Visibility.Collapsed;
-
-                case "10":
-                    return bValue ? Visibility.Hidden : Visibility.Visible;
-
-                case "12":
-                    return bValue ? Visibility.Hidden : Visibility.Collapsed;
-
-                case "20":
-                    return bValue ? Visibility.Collapsed : Visibility.Visible;
-
-                case "21":
-                    return bValue ? Visibility.Collapsed : Visibility.Hidden;
+                var itemControl = value as ItemsControl;
+                if (itemControl?.ItemsSource != null)
+                {
+                    list = itemControl.ItemsSource as IList;
+                    enumerable = itemControl.ItemsSource;
+                }
             }
+
+            if (list != null)
+            {
+                return list.Count == 0 ? Visibility.Collapsed : Visibility.Visible;
+            }
+
+            if (enumerable != null)
+            {
+                return enumerable.Cast<object>().Any() ? Visibility.Visible : Visibility.Collapsed;
+            }
+
+            return Visibility.Collapsed;
         }
 
         /// <summary>
@@ -50,9 +52,6 @@ namespace Jasily.Windows.Data.ValueConverters
         /// A converted value. If the method returns null, the valid null value is used.
         /// </returns>
         /// <param name="value">The value that is produced by the binding target.</param><param name="targetType">The type to convert to.</param><param name="parameter">The converter parameter to use.</param><param name="culture">The culture to use in the converter.</param>
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-            => value as Visibility? == Visibility.Visible;
-
-        #endregion
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => null;
     }
 }
