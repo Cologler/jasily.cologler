80358567a0c0d32636f748e8e84b9366ecd6bd22
赖巍 <skyoflw@gmail.com>
2016-07-05 17:20:59 +0800

    fix.

--- UnitTest.Desktop/Linq/UnitTestForJasilyExpression.cs
+++ UnitTest.Desktop/Linq/UnitTestForJasilyExpression.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace UnitTest.Desktop.Linq
 {
@@ -10,18 +10,18 @@ namespace UnitTest.Desktop.Linq
         [TestMethod]
         public void TestForGiveup()
         {
-            Assert.AreEqual(9, Enumerable.Range(0, 10).Giveup(1).Count());
-            Assert.AreEqual(0, Enumerable.Range(0, 1).Giveup(1).Count());
-            Assert.AreEqual(0, Enumerable.Empty<int>().Giveup(1).Count());
-            Assert.AreEqual(7, Enumerable.Range(0, 10).Giveup(2).Last());
+            Assert.AreEqual(9, Enumerable.Range(0, 10).GiveUp(1).Count());
+            Assert.AreEqual(0, Enumerable.Range(0, 1).GiveUp(1).Count());
+            Assert.AreEqual(0, Enumerable.Empty<int>().GiveUp(1).Count());
+            Assert.AreEqual(7, Enumerable.Range(0, 10).GiveUp(2).Last());
 
-            Assert.AreEqual(9, Enumerable.Range(0, 10).ToList().Giveup(1).Count());
-            Assert.AreEqual(0, Enumerable.Range(0, 1).ToList().Giveup(1).Count());
-            Assert.AreEqual(0, Enumerable.Empty<int>().ToList().Giveup(1).Count());
-            Assert.AreEqual(7, Enumerable.Range(0, 10).ToList().Giveup(2).Last());
+            Assert.AreEqual(9, Enumerable.Range(0, 10).ToList().GiveUp(1).Count());
+            Assert.AreEqual(0, Enumerable.Range(0, 1).ToList().GiveUp(1).Count());
+            Assert.AreEqual(0, Enumerable.Empty<int>().ToList().GiveUp(1).Count());
+            Assert.AreEqual(7, Enumerable.Range(0, 10).ToList().GiveUp(2).Last());
 
-            Assert.AreEqual("234", new string("12345".Skip(1).Giveup(1).ToArray()));
-            Assert.AreEqual("234", "12345".Skip(1).Giveup(1).GetString());
+            Assert.AreEqual("234", new string("12345".Skip(1).GiveUp(1).ToArray()));
+            Assert.AreEqual("234", "12345".Skip(1).GiveUp(1).GetString());
         }
     }
 }
