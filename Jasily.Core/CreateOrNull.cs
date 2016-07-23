4d7cfa226ec398edf564cc5ba292f9ad81e95f04
赖巍 <skyoflw@gmail.com>
2016-07-23 12:50:34 +0800

    feat: creator.

---
+++ Jasily.Core/CreateOrNull.cs
@@ -0,0 +1,11 @@
+﻿namespace System
+{
+    public static class CreateOrNull
+    {
+        public static Uri CreateUri(string uriString, UriKind uriKind)
+        {
+            Uri uri;
+            return Uri.TryCreate(uriString, uriKind, out uri) ? uri : null;
+        }
+    }
+}
