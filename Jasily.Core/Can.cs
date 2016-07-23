57c4480d3072c1ab6b74efb06ac25edea177eea9
赖巍 <skyoflw@gmail.com>
2016-07-23 11:43:50 +0800

    feat: test class

---
+++ Jasily.Core/Can.cs
@@ -0,0 +1,11 @@
+﻿namespace System
+{
+    public static class Can
+    {
+        public static bool CreateUri(string uriString, UriKind uriKind)
+        {
+            Uri uri;
+            return Uri.TryCreate(uriString, uriKind, out uri);
+        }
+    }
+}
