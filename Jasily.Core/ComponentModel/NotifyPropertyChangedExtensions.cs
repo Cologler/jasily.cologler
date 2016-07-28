12cf4b5a836559a066bea0dbae1f7694bfa4a75a
赖巍 <skyoflw@gmail.com>
2016-07-28 16:34:12 +0800

    refactor.

---
+++ Jasily.Core/ComponentModel/NotifyPropertyChangedExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using JetBrains.Annotations;
+
+namespace System.ComponentModel
+{
+    public static class NotifyPropertyChangedExtensions
+    {
+        public static void Invoke([NotNull] this PropertyChangedEventHandler handler, object sender, string propertyName)
+            => handler.Invoke(sender, new PropertyChangedEventArgs(propertyName));
+
+        /// <summary>
+        /// if e != null, call e() with propertyName
+        /// </summary>
+        /// <param name="handler"></param>
+        /// <param name="sender"></param>
+        /// <param name="propertyName"></param>
+        public static void Fire([CanBeNull] this PropertyChangedEventHandler handler, object sender, string propertyName)
+            => handler?.Invoke(sender, new PropertyChangedEventArgs(propertyName));
+
+        /// <summary>
+        /// if e != null, call e() with mulit propertyNames
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="handler"></param>
+        /// <param name="sender"></param>
+        /// <param name="propertyNames"></param>
+        public static void Fire([CanBeNull] this PropertyChangedEventHandler handler, object sender, params string[] propertyNames)
+        {
+            if (handler == null) return;
+            foreach (var propertyName in propertyNames)
+                handler(sender, new PropertyChangedEventArgs(propertyName));
+        }
+
+        /// <summary>
+        /// if e != null, call e() with mulit propertyNames
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="handler"></param>
+        /// <param name="sender"></param>
+        /// <param name="propertyNames"></param>
+        public static void Fire([CanBeNull] this PropertyChangedEventHandler handler, object sender, IEnumerable<string> propertyNames)
+        {
+            if (handler == null) return;
+            foreach (var propertyName in propertyNames)
+                handler(sender, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- README.md
+++ README.md
@@ -10,3 +10,9 @@ Jasily.Core.CSShard|none|**do not use it**
 Jasily.Core.Desktop|desktop|link from Jasily.Core.CSShard
 Jasily.Core.WP80|windows phone 8.0|link from Jasily.Core.CSShard
 Jasily.Core.WP81|windows phone 8.1|link from Jasily.Core.CSShard
+
+## dev
+
+### when use `throw new ArgumentNullException();`
+
+only for path safe.
