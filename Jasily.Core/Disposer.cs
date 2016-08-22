462beb0874b95589dd1429a863a3c3e3c52b1d2c
赖巍 <skyoflw@gmail.com>
2016-08-22 12:34:19 +0800

    refactor.

---
+++ Jasily.Core/Disposer.cs
@@ -0,0 +1,17 @@
+﻿using JetBrains.Annotations;
+
+namespace System
+{
+    public struct Disposer : IDisposable
+    {
+        private readonly Action target;
+
+        public Disposer([NotNull] Action target)
+        {
+            if (target == null) throw new ArgumentNullException(nameof(target));
+            this.target = target;
+        }
+
+        public void Dispose() => this.target();
+    }
+}
